42eda225f1e7296754de10bd44d342e5dce0e970
hkarargi <hussein@ahkfastrack.com>
2023-06-12 19:54:46 +0300

    Add project files.

---
+++ ModHelperData.cs
@@ -0,0 +1,18 @@
+﻿namespace Multitool;
+
+public static class ModHelperData
+{
+    public const string WorksOnVersion = "37.0";
+    public const string Version = "1.0.0";
+    public const string Name = "Multitool";
+
+    public const string Description = "A mod that lets you change the current round, the amount of cash you have, and the amount of lives you have!" +
+        "\nControls:" +
+        "\nF6: Change Round" +
+        "\nF7: Change Money" +
+        "\nF8: Change Health";
+
+    public const string RepoOwner = "hkarargi";
+    public const string RepoName = "Multitool";
+    public const string Icon = "Icon.png";
+}
---
+++ MultitoolMod.cs
@@ -0,0 +1,50 @@
+﻿using MelonLoader;
+using BTD_Mod_Helper;
+using BTD_Mod_Helper.Api;
+using BTD_Mod_Helper.Api.Display;
+using Multitool;
+using UnityEngine;
+using Il2CppAssets.Scripts.Unity.UI_New.Popups;
+using Il2CppAssets.Scripts.Unity.UI_New.InGame;
+
+[assembly: MelonInfo(typeof(MultitoolMod), Multitool.ModHelperData.Name, Multitool.ModHelperData.Version, Multitool.ModHelperData.RepoOwner)]
+[assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
+namespace Multitool;
+
+class MultitoolMod : BloonsTD6Mod
+{
+    public override void OnUpdate()
+    {
+        base.OnUpdate();
+        bool inActiveGame = InGame.instance != null && InGame.instance.bridge != null;
+        if (inActiveGame)
+        {
+            if (Input.GetKeyDown(KeyCode.F6))
+            {
+                Il2CppSystem.Action<int> wantedRound = (Il2CppSystem.Action<int>)delegate (int newRound)
+                { if (newRound > 0) { InGame.instance.bridge.SetRound(newRound - 1); } };
+
+                PopupScreen.instance.ShowSetValuePopup("Round",
+                    "What would you like to set the round to?", wantedRound, 3);
+            }
+
+            if (Input.GetKeyDown(KeyCode.F7))
+            {
+                Il2CppSystem.Action<int> wantedMoney = (Il2CppSystem.Action<int>)delegate (int newMoney)
+                { InGame.instance.bridge.SetCash(newMoney); };
+
+                PopupScreen.instance.ShowSetValuePopup("Cash",
+                    "What would you like to set your cash to?", wantedMoney, 650);
+            }
+
+            if (Input.GetKeyDown(KeyCode.F8))
+            {
+                Il2CppSystem.Action<int> wantedHealth = (Il2CppSystem.Action<int>)delegate (int newHealth)
+                { InGame.instance.bridge.SetSandboxHealth(newHealth); };
+
+                PopupScreen.instance.ShowSetValuePopup("Lives",
+                    "What would you like to set your health to?", wantedHealth, 100);
+            }
+        }
+    }
+} 
