08df6c258fbc73ce7cbc9e5682b5304ae463f852
Hussein El Karargi <hussein@ahkfastrack.com>
2024-11-25 12:26:46 +0200

    v45.3

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -2,15 +2,15 @@
 
 public static class ModHelperData
 {
-    public const string WorksOnVersion = "37.0";
-    public const string Version = "1.0.0";
+    public const string WorksOnVersion = "45.3";
+    public const string Version = "1.0.1";
     public const string Name = "Multitool";
 
     public const string Description = "A mod that lets you change the current round, the amount of cash you have, and the amount of lives you have!" +
         "\nControls:" +
-        "\nF6: Change Round" +
-        "\nF7: Change Money" +
-        "\nF8: Change Health";
+        "\nF5: Change Round" +
+        "\nF6: Change Money" +
+        "\nF7: Change Health";
 
     public const string RepoOwner = "hkarargi";
     public const string RepoName = "Multitool";
--- MultitoolMod.cs
+++ MultitoolMod.cs
@@ -15,11 +15,9 @@ class MultitoolMod : BloonsTD6Mod
 {
     public override void OnUpdate()
     {
-        base.OnUpdate();
-        bool inActiveGame = InGame.instance != null && InGame.instance.bridge != null;
-        if (inActiveGame)
+        if (InGame.instance != null && InGame.instance.bridge != null)
         {
-            if (Input.GetKeyDown(KeyCode.F6))
+            if (Input.GetKeyDown(KeyCode.F5))
             {
                 Il2CppSystem.Action<int> wantedRound = (Il2CppSystem.Action<int>)delegate (int newRound)
                 { if (newRound > 0) { InGame.instance.bridge.SetRound(newRound - 1); } };
@@ -28,7 +26,7 @@ class MultitoolMod : BloonsTD6Mod
                     "What would you like to set the round to?", wantedRound, 3);
             }
 
-            if (Input.GetKeyDown(KeyCode.F7))
+            if (Input.GetKeyDown(KeyCode.F6))
             {
                 Il2CppSystem.Action<int> wantedMoney = (Il2CppSystem.Action<int>)delegate (int newMoney)
                 { InGame.instance.bridge.SetCash(newMoney); };
@@ -37,7 +35,7 @@ class MultitoolMod : BloonsTD6Mod
                     "What would you like to set your cash to?", wantedMoney, 650);
             }
 
-            if (Input.GetKeyDown(KeyCode.F8))
+            if (Input.GetKeyDown(KeyCode.F7))
             {
                 Il2CppSystem.Action<int> wantedHealth = (Il2CppSystem.Action<int>)delegate (int newHealth)
                 { InGame.instance.bridge.SetSandboxHealth(newHealth); };
